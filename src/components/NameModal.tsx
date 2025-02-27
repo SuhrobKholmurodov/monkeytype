730b7bffa820f9d5f41f5e081e5d8ae642c63b54
Kholmurodov <suhrobkholmurodov1@gmail.com>
2025-02-27 22:53:27 +0500

    style(NameModal): apply clip-path to NameModal background

--- src/components/NameModal.tsx
+++ src/components/NameModal.tsx
@@ -20,26 +20,27 @@ const NameModal = ({ isOpen, onClose }: NameModalProps) => {
 
   return (
     <div className='fixed top-0 left-0 w-full h-full bg-black bg-opacity-60 flex justify-center items-center z-50'>
-      <div className='bg-white p-6 sm:p-4 rounded-lg shadow-lg text-center w-11/12 max-w-sm transition-all'>
-        <h2 className='text-xl text-gray-800 mb-4'>
-          Welcome to the Typing Test!
+      <div className='relative bg-white p-6 sm:p-4 rounded-lg shadow-lg text-center w-11/12 max-w-sm transition-all overflow-hidden'>
+        <div
+          className='absolute top-0 left-0 w-full h-full bg-[#22375d]'
+          style={{
+            clipPath: 'polygon(0 0, 9% 0, 58% 44%, 6% 100%, 0 100%, 0% 50%)'
+          }}
+        ></div>
+        <h2 className='text-2xl text-black mb-4 relative z-10'>
+          Typing Test!
         </h2>
-        <p className='text-gray-600 mb-6 text-base leading-relaxed'>
-          Enter your name to start the typing test. Your speed and accuracy will
-          be tracked.
-        </p>
-
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className='relative z-10'>
           <input
             type='text'
             value={localName}
             onChange={e => setLocalName(e.target.value)}
             placeholder='Enter your name'
-            className='p-3 mb-4 w-full rounded-md border border-gray-300 text-base focus:outline-none focus:ring-2 focus:ring-green-500'
+            className='p-3 mb-4 w-full rounded-md border border-gray-300 text-base focus:outline-none focus:ring-1 focus:ring-[#6d93d9]'
           />
           <button
             type='submit'
-            className='w-full p-3 bg-green-500 text-white rounded-md font-semibold hover:bg-green-600 focus:outline-none'
+            className='w-full p-3 bg-[#2c4779] text-white rounded-md font-semibold hover:bg-[#3f609c] focus:outline-none'
           >
             Start Typing Test
           </button>
